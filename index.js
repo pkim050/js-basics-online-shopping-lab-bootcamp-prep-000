5a7a0e1d6719dc068bbdbea41389fb5d228f3a34
Patrick A Kim <pkim050@gmail.com>
2018-11-09 19:29:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,6 +78,6 @@ function placeOrder(cardNumber) {
   else {
     var total = total();
     cart = [];
-    return "Your total cost is $" + total() + `, which will be charged to the card ${cardNumber}.`;
+    return "Your total cost is $" + total + `, which will be charged to the card ${cardNumber}.`;
   }
 }

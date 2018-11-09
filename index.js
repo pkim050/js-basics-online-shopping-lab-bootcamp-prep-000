b3e50605f725950c0329c3e1cc6889341729edc3
Patrick A Kim <pkim050@gmail.com>
2018-11-09 19:29:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else {
+    var total = total();
     cart = [];
     return "Your total cost is $" + total() + `, which will be charged to the card ${cardNumber}.`;
   }
